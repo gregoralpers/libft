76623a99292c4e8fbe02c893a531bf2d172a91e6
Gregor Alpers <gregoralpers@Gregors-MacBook-Pro-8690.local>
2022-02-19 15:32:12 +0000

    Bonus

---
+++ ft_lstmap.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: galpers <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 15:25:03 by galpers           #+#    #+#             */
+/*   Updated: 2022/02/19 15:31:46 by galpers          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*temp;
+
+	if (lst == NULL || f == NULL || del == NULL)
+		return (NULL);
+	new_list = ft_lstnew(f(lst->content));
+	temp = new_list;
+	while (lst->next != NULL)
+	{
+		lst = lst->next;
+		temp->next = ft_lstnew(f(lst->content));
+		temp = temp->next;
+		if (temp == NULL)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+	}
+	temp->next = NULL;
+	return (new_list);
+}
---
+++ ft_strtrim.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: galpers <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/18 18:39:20 by galpers           #+#    #+#             */
+/*   Updated: 2022/02/18 19:49:20 by galpers          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	if (ft_strlen(s1) <= 0)
+		return (NULL);
+	i = 0;
+	j = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[i]) != NULL)
+	{
+		if (i++ > j)
+			return (NULL);
+	}
+	while (ft_strchr(set, s1[j]) != NULL)
+	{
+		if (j-- <= i)
+			return (NULL);
+	}
+	return (ft_substr(s1, i, j + 1 - i));
+}
